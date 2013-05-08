b8c13f6fbb471f959a50267da81934a6a6c4699c
Rafael Miceli <rafael.miceli@hotmail.com>
2013-05-08 14:24:31 -0300

    Creating the site for my father

---
+++ WebSiteMjr/Models/EmployeeModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace WebSiteMjr.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext()
+            :base("DefaultConnection")
+        {
+            
+        }
+    }
+
+    public class Employee
+    {
+    }
+}
---
+++ WebSiteMjr/Models/SupportModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace WebSiteMjr.Models
+{
+    public class SupportContext: DbContext
+    {
+        public SupportContext()
+            : base("DefaultConnection")
+        {
+                
+        }
+    }
+
+    [Table("TIcket")]
+    public class SupportTicket
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int TicketId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public TicketStatus TicketStatus { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime TimeBegin { get; set; }
+        public DateTime TimeDone { get; set; }
+        public string FeedBack { get; set; }
+    }
+
+    public enum TicketStatus
+    {
+        Open,
+        Doing,
+        Done
+    }
+}
