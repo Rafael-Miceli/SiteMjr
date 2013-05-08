ade0e590df4a4509f67c2f31273cc86ab767d0df
Rafael Miceli <rafael.miceli@hotmail.com>
2013-05-08 18:46:25 -0300

    Update EmployeeModels.cs

--- WebSiteMjr/Models/EmployeeModels.cs
+++ WebSiteMjr/Models/EmployeeModels.cs
@@ -14,6 +14,8 @@ namespace WebSiteMjr.Models
 
     public class Employee
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
