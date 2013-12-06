ee52a3513c4518aa2d78df865292c41b47434ec4
Rafael Miceli <rafael.miceli@hotmail.com>
2013-12-06 18:15:13 -0200

    DropDown Menu

--- WebSiteMjr/App_Start/BundleConfig.cs
+++ WebSiteMjr/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace WebSiteMjr
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery")
                         .Include("~/Scripts/jquery-{version}.js")
-                        .Include("~/Scripts/jquery.maskedinput.js"));
+                        .Include("~/Scripts/jquery.maskedinput.js")
+                        .Include("~/bootstrap/js/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui")
                         .Include("~/Scripts/jquery-ui-{version}.js"));
@@ -23,7 +24,9 @@ namespace WebSiteMjr
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                        .Include("~/bootstrap/css/bootstrap.css")
+                        .Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
