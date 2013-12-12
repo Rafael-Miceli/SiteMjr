6d3de24a4dbd1700d3f64051f94cca19754c62c1
Rafael Miceli <rafael.miceli@hotmail.com>
2013-12-12 07:14:39 -0200

    Azeite no Javascript >.<

--- WebSiteMjr.Domain/Model/Employee.cs
+++ WebSiteMjr.Domain/Model/Employee.cs
@@ -10,7 +10,7 @@ namespace WebSiteMjr.Domain.Model
         public string Name { get; set; }
         [RegularExpression(@"^(\([0-9][0-9]\) [0-9]{5}-[0-9]{4})|(\(1[2-9]\) [5-9][0-9]{3}-[0-9]{4})|(\([2-9][1-9]\) [5-9][0-9]{3}-[0-9]{4})$", 
             ErrorMessage = "Telefone Inválido")]
-        [MaskPropertie(MaskType = "CelPhone")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:(##) #####-####}")]
         public virtual string Phone { get; set; }
         [Required(ErrorMessage = "Sobrenome é um campo obrigatório")]
         public virtual string LastName { get; set; }
--- WebSiteMjr/Scripts/Mask/Masks.js
+++ WebSiteMjr/Scripts/Mask/Masks.js
@@ -1,5 +1,13 @@
 ﻿$(function () {
-    $("#Phone").mask("(99) 9999-9999");
-    $("[mask-type='CelPhone']").mask("(99) 99999-9999");
+    // ==== Azeite no código ====
+    $(".editor-field #Phone").mask("(99) 9999-9999");
+    
+    
+    $(".editor-field1 #Phone").mask("(99) 99999-9999");
+    // ==== Azeite no código ====
+
+    //Ideal
+    //$("[mask-type='CelPhone']").mask("(99) 99999-9999");
+
     $("#CheckinDateTime_Date").mask("99/99/9999");
 });
