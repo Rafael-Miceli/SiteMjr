1fc0d64ea228de17242b6a7fd7eb454d1f78a64b
Rafael Miceli <rafael.miceli@hotmail.com>
2014-07-14 16:34:06 -0300

    Refactoring account module
    Still need to recator ViewModels locations and continue to complete Auth
    for companys

---
+++ WebSiteMjr.Domain/Interfaces/Membership/IMembershipService.cs
@@ -0,0 +1,14 @@
+﻿using WebSiteMjr.Domain.Interfaces.Membership;
+
+namespace WebSiteMjr.Domain.services.Membership
+{
+    public interface IMembershipService
+    {
+        bool Login(string username, string password, bool rememberMe = false);
+        void Logout();
+        void CreateAccount(IFlexMembershipUser user);
+        bool HasLocalAccount(string username);
+        bool ChangePassword(string username, string oldPassword, string newPassword);
+        void SetLocalPassword(string username, string newPassword);
+    }
+}
