5dcc4feaa7633d2a433554bd1780a7f6361b449b
Rafael Miceli <rafael.miceli@hotmail.com>
2014-07-21 20:29:23 -0300

    Reestructuring User Module
    Need to reestrocture the User Module

---
+++ WebSiteMjr.Domain.Test/Model/UserDummies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSiteMjr.Domain.Model.Membership;
+
+namespace WebSiteMjr.Domain.Test.Model
+{
+    public static class UserDummies
+    {
+        public static User ReturneOneMjrActiveUser()
+        {
+            var user = new User
+            {
+                Id = 1,
+                Email = "rafael.miceli@hotmail.com"
+            };
+
+            return user;
+        }
+
+        public static User ReturneOneNonMjrActiveUser()
+        {
+            var user = new User
+            {
+                Id = 1,
+                Email = "rafael.miceli@hotmail.com",
+                IdCompany = CompanyDummies.CreateOneCompany().Id,
+                IsLocal = true,
+                Name = "Rafael",
+                LastName = "Miceli",
+                Password = "",
+                StatusUser = StatusUser.Active,
+                Username = "rafael.miceli@hotmail.com"
+            };
+
+            return user;
+        }
+
+    }
+}
