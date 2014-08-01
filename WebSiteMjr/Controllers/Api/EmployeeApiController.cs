405b7175e723d5e1b34689f696bb27e12ff6d02d
Rafael Miceli <rafael.miceli@hotmail.com>
2014-08-01 17:01:40 -0300

    Hide create login button

---
+++ WebSiteMjr/Controllers/Api/EmployeeApiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebSiteMjr.Domain.Interfaces.Services;
+using WebSiteMjr.Domain.Model.Membership;
+
+namespace WebSiteMjr.Controllers.Api
+{
+    public class EmployeeApiController : ApiController
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMembershipService _membershipService;
+
+        public EmployeeApiController(IEmployeeService employeeService, IMembershipService membershipService)
+        {
+            _employeeService = employeeService;
+            _membershipService = membershipService;
+        }
+
+        //[HttpGet]
+        [AcceptVerbs("GET", "HEAD")]
+        public User GetEmployeeUser(string employeeId)
+        {
+            return  _membershipService.FindUserByEmployeeId(_employeeService.FindEmployee(employeeId).Id);
+        }
+    }
+}
---
+++ WebSiteMjr/Scripts/Employee/Edit.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var employeeLogin,
+        employeeVerifyLoginCall;
+
+    //Set Ajax call
+    employeeVerifyLoginCall = {
+        url: '/Api/EmployeeApi/GetEmployeeUser?employeeId=' + $('#Id').val(),
+        type: 'GET',
+        datatype: 'json'
+    };
+
+    //Make Ajax call
+
+    $.ajax(employeeVerifyLoginCall)
+        .then(employeeVerifyLoginCallSucceded)
+        .fail(employeeVerifyLoginCallFailed);
+
+
+
+    function employeeVerifyLoginCallSucceded(data) {
+        employeeLogin = data;
+
+        if (employeeLogin != 'undefined' || employeeLogin != null) {
+            alert('Suma com o botão');
+        }
+    }
+
+    function employeeVerifyLoginCallFailed(textStatus) {
+        alert('Error chamada ajax. ' + textStatus);
+    }
+});
