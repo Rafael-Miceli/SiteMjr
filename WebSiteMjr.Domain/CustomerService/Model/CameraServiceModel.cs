f770c6ea449b0e2faf10479a284b3f3fbb8d7557
Rafael Miceli <rafael.miceli@hotmail.com>
2014-09-08 11:19:40 -0300

    Re-Thinking the servicetype model

--- WebSiteMjr.Domain/CustomerService/Model/CameraServiceModel.cs
+++ WebSiteMjr.Domain/CustomerService/Model/CameraServiceModel.cs
@@ -4,6 +4,15 @@ namespace WebSiteMjr.Domain.CustomerService.Model
 {
     public class CameraServiceModel : ServiceType
     {
-        public IEnumerable<string> CameraServerName { get; set; }
+        public class CameraServer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Channels { get; set; }
+        }
+
+        public IEnumerable<CameraServer> CameraServers { get; set; }
+
+
     }
 }
