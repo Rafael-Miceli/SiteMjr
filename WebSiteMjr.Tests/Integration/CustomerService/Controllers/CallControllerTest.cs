6554035e8d6b6ecb9f5a1ae9d6e6ee894ff1746c
Rafael Miceli <rafael.miceli@hotmail.com>
2014-09-09 22:38:00 -0300

    Commit

---
+++ WebSiteMjr.Tests/Integration/CustomerService/Controllers/CallControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebSiteMjr.Areas.CustomerService.Controllers;
+
+namespace WebSiteMjr.Tests.Integration.CustomerService.Controllers
+{
+    [TestClass]
+    public class CallControllerTest
+    {
+        [TestMethod]
+        public void Given_A_Request_To_Inde_Page_When_Calling_Index_Page_Then_Return_List_Of_Pedent_And_IntoService_Calls()
+        {
+            var callController = new CallController(null);
+
+            var result = callController.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
